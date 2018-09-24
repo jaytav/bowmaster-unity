7cacd94c24acfe10adba455efbe1f870a08bc2d7
tavaresj <jayden.tavares@hotmail.com>
2018-09-24 21:06:40 +1000

    Create and appy playershooting script to player and create arrow prefab

---
+++ Bow Master (Game)/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShooting : MonoBehaviour
+{
+	public static float chargeTime = 0f; //how long the player has charged the bow
+	public GameObject arrow; //arrow being spawned
+
+	const float maxCharge = 1f; //maximum charge time
+
+	private Transform arrowSpawn;
+
+	void Start()
+	{
+		arrowSpawn = transform;
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			//increase charge time based on time held
+			chargeTime += Time.deltaTime;
+			
+			//limit maximum charge time to 1 second
+			if (chargeTime > maxCharge) { chargeTime = maxCharge; }
+		}
+
+		if (Input.GetButtonUp("Fire1"))
+		{	
+			if (chargeTime >= 1f) //if bow charged for 1 second
+			{
+				//create an instance of the arrow, located on arrowSpawn
+				Instantiate(arrow, arrowSpawn.position, arrowSpawn.rotation);
+			}
+			
+			chargeTime = 0f; //reset charge time back to 0
+		}
+	}
+}
