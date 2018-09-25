253faed5d532d8c0a34bb3fdfd24e2b8d94e2497
tavaresj <jayden.tavares@hotmail.com>
2018-09-25 22:57:28 +1000

    Create charge bar above player with script

---
+++ Bow Master (Game)/Assets/Scripts/Player/ChargeBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeBar : MonoBehaviour {
+
+	private Image chargeBar;
+
+	void Awake() {
+		chargeBar = GetComponent<Image>();
+	}
+
+	void Update() {
+		chargeBar.fillAmount = PlayerShooting.chargeTime;
+	}
+}
