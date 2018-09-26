0c26e271ac73b95d477df131f668890d3dc10fdb
tavaresj <jayden.tavares@hotmail.com>
2018-09-26 22:00:21 +1000

    Added Blue Brick edges, changed enemy edge check

--- Bow Master (Game)/Assets/Scripts/Enemy/EdgeCheck.cs
+++ Bow Master (Game)/Assets/Scripts/Enemy/EdgeCheck.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class EdgeCheck : MonoBehaviour {
 
-	void OnTriggerExit2D(Collider2D col) {
-		StartCoroutine(EdgeWait());
-		EnemyMovement.direction *= -1f;
+	public float waitLength = 2f;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Ground Edge") {
+			StartCoroutine(EdgeWait());
+			EnemyMovement.direction *= -1f;
+		}
 	}
 
 	IEnumerator EdgeWait() { //stops enemy moving for 3 seconds
 		EnemyMovement.isMoving = false;
-		yield return new WaitForSeconds(3f);
+		yield return new WaitForSeconds(waitLength);
 		EnemyMovement.isMoving = true;
 	}
 }
