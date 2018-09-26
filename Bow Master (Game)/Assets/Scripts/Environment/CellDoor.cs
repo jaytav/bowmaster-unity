21397108de5c33d21d18ca6f552d93386e56ae06
tavaresj <jayden.tavares@hotmail.com>
2018-09-27 00:11:51 +1000

    Create Cell Door which opens when player comes in trigger

---
+++ Bow Master (Game)/Assets/Scripts/Environment/CellDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellDoor : MonoBehaviour {
+
+	public float speed;
+	public float time;
+
+	private Transform target;
+	private Collider2D cellDoorCol;
+	private bool opened = false;
+
+	void Start() {
+		target = GetComponent<Transform>();
+		cellDoorCol = GetComponent<Collider2D>();
+		target.position = new Vector3(transform.position.x, 3f, 0f);
+	}
+
+	void Update() {
+		if (opened) {
+			transform.Translate(0f, speed * Time.deltaTime, 0f);
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			StartCoroutine(OpenClose());
+		}
+	}
+
+	IEnumerator OpenClose() {
+		opened = true;
+		yield return new WaitForSeconds(time);
+		cellDoorCol.enabled = false;
+		opened = false;
+	}
+}
---
+++ Bow Master (Game)/Assets/Scripts/Environment/CreateNumberOfPlatforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNumberOfPlatforms : MonoBehaviour {
+
+	public GameObject[] platformObjects;
+	public int yDistance;
+
+	private Transform parentPosition;
+
+	void Start() {
+		parentPosition = GetComponent<Transform>();
+		parentPosition = parentPosition.transform;
+
+		for (int i = 0; i < platformObjects.Length; i++) {
+			int randomNum = Random.Range(0, platformObjects.Length - 1);
+
+			Instantiate(platformObjects[randomNum], 
+						new Vector2(parentPosition.position.x, yDistance * i), 
+						transform.rotation);
+		}
+	}
+}
