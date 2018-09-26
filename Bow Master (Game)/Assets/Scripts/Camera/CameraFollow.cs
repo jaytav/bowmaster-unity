25bf286694166373145775797e3f0b70e34fffbb
tavaresj <jayden.tavares@hotmail.com>
2018-09-26 20:10:58 +1000

    CameraFollow script to follow player movement

---
+++ Bow Master (Game)/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+	void LateUpdate() {
+		if (target) {
+			Vector3 desiredPosition = target.position + offset;
+			transform.position = desiredPosition;
+		}
+	}
+}
