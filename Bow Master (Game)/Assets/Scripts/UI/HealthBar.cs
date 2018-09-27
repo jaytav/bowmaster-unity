e5810764bbc88e2dea2a8361c1a7f9690c55be7b
tavaresj <jayden.tavares@hotmail.com>
2018-09-28 00:59:59 +1000

    Create screen UI with functional player health

---
+++ Bow Master (Game)/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	public Slider healthBar;
+	public Text healthNum;
+
+	private GameObject player;
+	private PlayerHealth playerHealth;
+
+	void Start() {
+		player = GameObject.FindWithTag("Player");
+		playerHealth = player.GetComponent<PlayerHealth>();
+
+		healthBar.maxValue = playerHealth.startingHealth;
+	}
+
+	void Update() {
+		healthBar.value = playerHealth.currentHealth;
+
+		healthNum.text = playerHealth.currentHealth + "/" + playerHealth.startingHealth;
+	}
+}
