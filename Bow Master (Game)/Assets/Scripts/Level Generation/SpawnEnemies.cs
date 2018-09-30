827f80e37a686f1c06904477467585dfa6da0de5
tavaresj <jayden.tavares@hotmail.com>
2018-09-30 19:05:51 +1000

    Created object and script which randomly spawns a certain amount of rooms and spaces them out

--- Bow Master (Game)/Assets/Scripts/Level Generation/SpawnEnemies.cs
+++ Bow Master (Game)/Assets/Scripts/Level Generation/SpawnEnemies.cs
@@ -10,6 +10,7 @@ public class SpawnEnemies : MonoBehaviour {
 	void Start() {
 		for (int i = 0; i < enemySpawns.Length; i++) {
 			int enemyNum = Random.Range(0, enemies.Length);
+			
 			Instantiate(enemies[enemyNum], 
 						enemySpawns[i].position, 
 						enemySpawns[i].rotation);
---
+++ Bow Master (Game)/Assets/Scripts/Level Generation/SpawnRooms.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRooms : MonoBehaviour {
+
+	public GameObject[] rooms;
+
+	public float roomHeight;
+	public float roomLength;
+
+	private float currentRoomLength;
+	
+	void Start() {
+		currentRoomLength = roomLength/2;
+
+		for (int i = 0; i < rooms.Length; i++) {
+			int roomNum = Random.Range(0, rooms.Length);
+
+			Vector2 currentRoomPos = 
+				new Vector2(transform.position.x + currentRoomLength,
+							transform.position.y + roomHeight);
+
+			Instantiate(rooms[roomNum],
+						currentRoomPos,
+						transform.rotation);
+
+			currentRoomLength += roomLength;
+		}
+	}
+}
