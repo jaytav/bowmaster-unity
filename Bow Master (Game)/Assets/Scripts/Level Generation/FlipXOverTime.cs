790a452bcde3f2e7902c47f78ff6492b63dbdb48
tavaresj <jayden.tavares@hotmail.com>
2018-09-30 16:16:40 +1000

    Created a script to spawn random enemies in a number of spawn points. Made prefab of a level

---
+++ Bow Master (Game)/Assets/Scripts/Level Generation/FlipXOverTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipXOverTime : MonoBehaviour {
+
+	public float waitTime;
+
+	private float time;
+	private SpriteRenderer spriteRenderer;
+
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		time += Time.deltaTime;
+
+		if (time > waitTime) {
+			FlipX();
+			time = 0f;
+		}
+	}
+
+	void FlipX() {
+		if (spriteRenderer.flipX) {
+			spriteRenderer.flipX = false;
+		}
+		else spriteRenderer.flipX = true;
+	}
+}
---
+++ Bow Master (Game)/Assets/Scripts/Level Generation/SpawnEnemies.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour {
+	
+	public GameObject[] enemies;
+	public Transform[] enemySpawns;
+
+	void Start() {
+		for (int i = 0; i < enemySpawns.Length; i++) {
+			int enemyNum = Random.Range(0, enemies.Length);
+			Instantiate(enemies[enemyNum], 
+						enemySpawns[i].position, 
+						enemySpawns[i].rotation);
+		}
+	}
+}
