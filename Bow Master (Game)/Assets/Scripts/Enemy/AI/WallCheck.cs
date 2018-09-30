69420b71b5f85c74834ebe30cfe1ea8824d75155
tavaresj <jayden.tavares@hotmail.com>
2018-09-30 14:21:25 +1000

    Fixed enemy projectile behaviour, created new AI to detect walls in front, and created level 01 design

---
+++ Bow Master (Game)/Assets/Scripts/Enemy/AI/WallCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCheck : MonoBehaviour {
+
+	public float waitLength = 2f;
+	public EnemyMovement enemyMovement;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Ground") {
+			StartCoroutine(WallWait());
+			enemyMovement.direction *= -1;
+		}
+	}
+
+	IEnumerator WallWait() { //stops enemy moving for 3 seconds
+		enemyMovement.isMoving = false;
+		yield return new WaitForSeconds(waitLength);
+		enemyMovement.isMoving = true;
+	}
+}
