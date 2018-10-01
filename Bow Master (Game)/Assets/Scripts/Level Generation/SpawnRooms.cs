3646ab2a98f668eea72879d856ab345335500887
tavaresj <jayden.tavares@hotmail.com>
2018-10-01 14:41:45 +1000

    Added store into SpawnRooms script to spawn it between each room

--- Bow Master (Game)/Assets/Scripts/Level Generation/SpawnRooms.cs
+++ Bow Master (Game)/Assets/Scripts/Level Generation/SpawnRooms.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnRooms : MonoBehaviour {
 
 	public GameObject[] rooms;
+	public GameObject store;
 
 	public float roomHeight;
 	public float roomLength;
@@ -17,12 +18,22 @@ public class SpawnRooms : MonoBehaviour {
 		for (int i = 0; i < rooms.Length; i++) {
 			int roomNum = Random.Range(0, rooms.Length);
 
-			Vector2 currentRoomPos = 
+			Vector2 currentRoomPos = //position of room
 				new Vector2(transform.position.x + currentRoomLength,
 							transform.position.y + roomHeight);
 
-			Instantiate(rooms[roomNum],
+			Instantiate(rooms[roomNum], //spawn room
 						currentRoomPos,
+						transform.rotation);
+
+			currentRoomLength += roomLength;
+
+			currentRoomPos = //position of store
+				new Vector2(transform.position.x + currentRoomLength,
+							transform.position.y + roomHeight);
+
+			Instantiate(store, //spawn store
+						currentRoomPos, 
 						transform.rotation);
 
 			currentRoomLength += roomLength;
