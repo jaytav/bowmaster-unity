06369b8fca556b6d71da6170f7f4d244a536c6ab
tavaresj <jayden.tavares@hotmail.com>
2018-10-05 13:13:41 +1000

    Created new door design, animations. Created door prefab.

---
+++ Bow Master (Game)/Assets/Scripts/Environment/DoorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	public float waitTime;
+	public BoxCollider2D doorCol;
+
+	private bool isOpened;
+	private Animator doorAnim;
+	private BoxCollider2D doorTrigger;
+
+	void Start() {
+		isOpened = false; //door spawns closed
+		doorAnim = GetComponent<Animator>();
+		doorTrigger = GetComponent<BoxCollider2D>();
+	}
+
+	void Update() {
+		if (isOpened) {
+			doorAnim.SetBool("IsOpened", isOpened);
+			StartCoroutine(WaitDoorOpen());
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag == "Player" || col.tag == "Projectile") {
+			isOpened = true;
+		}
+	}
+
+	IEnumerator WaitDoorOpen() {
+		yield return new WaitForSeconds(waitTime);
+		doorCol.enabled = false;
+		doorTrigger.enabled = false;
+	}
+}
