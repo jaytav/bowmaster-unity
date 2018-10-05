f2fd852a5c4367afa88513b70110c588194a4a7e
tavaresj <jayden.tavares@hotmail.com>
2018-10-05 13:34:47 +1000

    Created spawndoor script on each room with door spawn points of new door prefab.

---
+++ Bow Master (Game)/Assets/Scripts/Level Generation/SpawnDoors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDoors : MonoBehaviour {
+
+	public GameObject door;
+	public Transform entrance;
+	public Transform exit;
+
+	void Start() {
+		Instantiate(door,
+					entrance.position,
+					entrance.rotation);
+
+		Instantiate(door,
+					exit.position,
+					exit.rotation);
+	}
+}
