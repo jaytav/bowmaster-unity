d09cc1712fb909ea32fcc6da71d52f8b821e249e
tavaresj <jayden.tavares@hotmail.com>
2018-10-17 15:05:03 +1100

    Created launch menu with animations and new art. Also created UIFlash script.

---
+++ Bow Master (Game)/Assets/Scripts/UI/UIFlash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFlash : MonoBehaviour {
+
+	public float flashRate;
+	public float flashLength;
+	public Color flashColor;
+
+	private Color normalColor;
+	public Text uiText;
+	private float counter;
+
+	void Start() {
+		uiText = GetComponent<Text>();
+		if (uiText) { normalColor = uiText.color; }
+	}
+
+	void Update() {
+		counter += Time.deltaTime;
+
+		if (counter > flashRate) {
+			StartCoroutine(WaitForFlash());
+		}
+	}
+
+	IEnumerator WaitForFlash() {
+		uiText.color = flashColor;
+		yield return new WaitForSeconds(flashLength);
+		uiText.color = normalColor;
+		counter = 0f;
+	}
+}
