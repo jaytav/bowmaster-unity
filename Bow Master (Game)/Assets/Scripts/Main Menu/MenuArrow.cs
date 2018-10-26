9697733f4795d8f2207d0bb1b28438a010394661
tavaresj <jayden.tavares@hotmail.com>
2018-10-26 18:49:12 +1100

    Menu Changes

--- Bow Master (Game)/Assets/Scripts/Main Menu/MenuArrow.cs
+++ Bow Master (Game)/Assets/Scripts/Main Menu/MenuArrow.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class MenuArrow : MonoBehaviour {
 
 	public float speed;
+	public AudioClip arrowBreakClip;
+
 	private BoxCollider2D arrowCol;
 	
 	void Start() {
@@ -19,6 +21,7 @@ public class MenuArrow : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "Button") {
 			arrowCol.enabled = false;
+			SoundManager.instance.PlaySingle(arrowBreakClip);
 		}
 	}
 }
